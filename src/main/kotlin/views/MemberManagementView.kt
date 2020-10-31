4a110e1b210d206a1a558e0ab72917db93c76b82
AindriuBrennan <20079247@mail.wit.ie>
2020-10-31 13:35:42 +0000

    Member Management View created, button on landing will access this view

---
+++ src/main/kotlin/views/MemberManagementView.kt
@@ -0,0 +1,43 @@
+package views
+import tornadofx.*
+
+class MemberManagementView: View() {
+    override val root = vbox{
+        setPrefSize(600.00,400.00)
+
+        form {
+            fieldset("Create Character") {
+                field("Name") {
+                    textfield()
+            }
+
+                field("Race") {
+                    textfield()
+                }
+
+                field("Class") {
+                   textfield()
+                }
+            }
+            button("Add Character") {
+                action{ println("Character Created!")}
+            }
+            button("Delete Character"){
+                action{ println("Character Deleted")}
+            }
+            button("Search Character"){
+                //implement search functionality
+            }
+            button("Main Menu") {
+                action {
+                    replaceWith<Landing>(
+                            ViewTransition.Slide(0.4.seconds,ViewTransition.Direction.RIGHT),
+                            true,
+                            true
+                    )
+                }
+            }
+        }
+    }
+
+}
