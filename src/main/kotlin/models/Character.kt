7845613fc6d497f2ff23d898210ad10efef5c482
AindriuBrennan <20079247@mail.wit.ie>
2020-10-31 17:58:04 +0000

    Basic Character/player model added

---
+++ src/main/kotlin/models/Character.kt
@@ -0,0 +1,10 @@
+package models
+
+import java.util.*
+
+class Character (
+     val name: String = "",
+     val race: String = "",
+     val classType: String = "",
+     val itemsCollected: List<Items>,
+)
